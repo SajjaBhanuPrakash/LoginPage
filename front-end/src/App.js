520633141fc5ee563f4a4c1c05322a467fd2ab49
Daniel Samson Raj Dowrla <dowrlad@amazon.com>
2021-04-06 13:41:05 +0530

    API definition created

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from  './components/home'
+import Login from './components/Login'
 import Main from './components/Main/Main'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Home /> */}
+         <Home />
         <Main />
       </div>
     );
